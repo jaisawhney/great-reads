b47bcc998a8666ed9155fea7dd0804d42e6a5668
Salo <salomonco@gmail.com>
2022-09-07 15:25:39 -0500

    corrected some functions

--- pages/add-friend.js
+++ pages/add-friend.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function MyBooks() {
+export default function AddFriend() {
   return (
     <div className={styles.container}>
       <Head>
