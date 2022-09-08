80ea28eb318800b82b63db4ff6ff27e63b996642
Jai Sawhney <jai.sawhney1@gmail.com>
2022-09-08 15:08:05 -0700

    Use the book cover as a thumbnail

--- pages/book-search.js
+++ pages/book-search.js
@@ -35,10 +35,14 @@ export default function BookSearch() {
                     searchResults
                         .filter(({title, author_name}) => title && author_name)
                         .map((result, i) => {
-                            const {title, author_name} = result;
+                            const {title, author_name, cover_i} = result;
+                            const cover = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
 
                             return (
                                 <div className="row-auto flex flex-row items-center shadow-md rounded" key={i}>
+                                    <img src={cover}
+                                         alt="Book thumbnail"
+                                         className="fill-current w-auto rounded-t h-auto"/>
                                     <div className="flex flex-col justify-between leading-normal p-3">
                                         <p className="font-bold text-md md:text-xl">{title}</p>
                                         <p className="text-base text-gray-700">{author_name.join(", ")}</p>
