9c4d6d6c08b2c36e23bba5f1eca97216c2a58634
Jai Sawhney <jai.sawhney1@gmail.com>
2022-09-19 13:12:51 -0700

    Update the API route for book creation

--- pages/api/books/index.js
+++ pages/api/books/index.js
@@ -1,17 +1,38 @@
-import prisma from '../../../lib/prisma';
-
-const sampleBook = {title: 'Catching Fire', key: '203943422'}
+import prisma from "../../../lib/prisma";
 
 export default async function handler(req, res) {
-  if (req.method=='GET'){
+  if (req.method === "GET") {
     const allBooks = await prisma.book.findMany();  // TODO: Add pagination
 
-    res.status(200).json(allBooks)
+    res.status(200).json(allBooks);
   } else {
-    const newBook = await prisma.book.create({
-      data: sampleBook,
+    // The /search route provides more information than the /works route
+    const result = await fetch(`http://openlibrary.org/search.json?q=key:${JSON.parse(req.body).olID}&limit=1`);
+    const bookObj = (await result.json())?.docs[0];
+    const bookListBook = await prisma.BookListBook.create({
+      data: {
+        bookList: {
+          connect: {
+            id: 1 //sample id
+          }
+        },
+        book: {
+          connectOrCreate: {
+            where: {
+              OLID: bookObj.key
+            },
+            create: {
+              OLID: bookObj.key,
+              title: bookObj.title,
+              pageCount: bookObj.number_of_pages_median.toString(),
+              publishedDate: bookObj.first_publish_year.toString()
+            }
+          }
+        }
+      },
+      include: { book: true }
     });
 
-    res.status(200).json(newBook)
+    res.status(201).json(bookListBook.book || {});
   }
 }
