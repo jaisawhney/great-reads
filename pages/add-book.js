65f28942d782d9f4906ed99bbac31f70a3aa4d0c
Jai Sawhney <jai.sawhney1@gmail.com>
2022-09-20 18:57:59 -0700

    Check if the book already exists in the shelf

--- pages/add-book.js
+++ pages/add-book.js
@@ -1,11 +1,10 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import {useState} from "react";
+import { useState } from "react";
 import BookList from "../components/BookList";
-import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-
-export default withPageAuthRequired(AddBook)
+export default withPageAuthRequired(AddBook);
 
 function AddBook() {
     const [searchResults, setSearchResults] = useState([]);
@@ -14,25 +13,29 @@ function AddBook() {
         e.preventDefault();
 
         const bookTitle = e.target.elements.bookName?.value;
-        if(!bookTitle) return;
+        if (!bookTitle) return;
 
         fetch(`/api/search?q=${bookTitle}`)
-            .then(res => res.json())
-            .then(res => {
+            .then((res) => res.json())
+            .then((res) => {
                 return setSearchResults(res.docs);
             });
     }
 
     function addToShelf(olID) {
-      fetch(`/api/books`, {
-        method:"POST",
-        body: JSON.stringify({
-          olID: olID
-        })
-      })
-        .then(res => res.json())
-        .then(res => {
-          //console.log(res);
+        fetch(`/api/books`, {
+            method: "POST",
+            body: JSON.stringify({
+                olID: olID,
+            }),
+        }).then((res) => {
+            // TODO: Replace the lines below
+            console.log(res.status);
+            if (res.status === 200) {
+                alert("Book already in shelf!");
+            } else {
+                alert("Book added!");
+            }
         });
     }
 
@@ -40,16 +43,22 @@ function AddBook() {
         <div className={styles.container}>
             <Head>
                 <title>Book Search</title>
-                <link rel="icon" href="/favicon.ico"/>
+                <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className={styles.main}>
                 <form onSubmit={onSubmit} className="border rounded shadow-sm">
-                    <input className="p-2" name="bookName" type="text" placeholder="Book title" required/>
-                    <input className="px-2" type="submit"/>
+                    <input
+                        className="p-2"
+                        name="bookName"
+                        type="text"
+                        placeholder="Book title"
+                        required
+                    />
+                    <input className="px-2" type="submit" />
                 </form>
                 <div className="grid grid-cols-1 gap-4 xl:grid-cols-3">
-                  <BookList books={searchResults} addToShelf={addToShelf} />
+                    <BookList books={searchResults} addToShelf={addToShelf} />
                 </div>
             </main>
         </div>
--- pages/api/books/index.js
+++ pages/api/books/index.js
@@ -1,38 +1,55 @@
 import prisma from "../../../lib/prisma";
 
+const bookListId = 1; // Sample book list ID
+
 export default async function handler(req, res) {
-  if (req.method === "GET") {
-    const allBooks = await prisma.book.findMany();  // TODO: Add pagination
+    if (req.method === "GET") {
+        const allBooks = await prisma.book.findMany(); // TODO: Add pagination
 
-    res.status(200).json(allBooks);
-  } else {
-    // The /search route provides more information than the /works route
-    const result = await fetch(`http://openlibrary.org/search.json?q=key:${JSON.parse(req.body).olID}&limit=1`);
-    const bookObj = (await result.json())?.docs[0];
-    const bookListBook = await prisma.BookListBook.create({
-      data: {
-        bookList: {
-          connect: {
-            id: 1 //sample id
-          }
-        },
-        book: {
-          connectOrCreate: {
+        res.status(200).json(allBooks);
+    } else {
+        // The /search route provides more information than the /works route
+        const result = await fetch(
+            `http://openlibrary.org/search.json?q=key:${JSON.parse(req.body).olID}&limit=1`
+        );
+        const bookObj = (await result.json())?.docs[0];
+
+        // Check if the book already exists in the shelf
+        const existingBook = await prisma.BookListBook.findFirst({
             where: {
-              OLID: bookObj.key
+                bookListId: bookListId,
+                OLID: bookObj.key,
             },
-            create: {
-              OLID: bookObj.key,
-              title: bookObj.title,
-              pageCount: bookObj.number_of_pages_median.toString(),
-              publishedDate: bookObj.first_publish_year.toString()
-            }
-          }
-        }
-      },
-      include: { book: true }
-    });
+        });
 
-    res.status(201).json(bookListBook.book || {});
-  }
+        // Book exists (200)
+        if (existingBook) return res.status(200).end();
+
+        // Add the book to the shelf and create a book if needed
+        await prisma.BookListBook.create({
+            data: {
+                bookList: {
+                    connect: {
+                        id: bookListId,
+                    },
+                },
+                book: {
+                    connectOrCreate: {
+                        where: {
+                            OLID: bookObj.key,
+                        },
+                        create: {
+                            OLID: bookObj.key,
+                            title: bookObj.title,
+                            pageCount: bookObj.number_of_pages_median.toString(),
+                            publishedDate: bookObj.first_publish_year.toString(),
+                        },
+                    },
+                },
+            },
+        });
+
+        // Book added (201)
+        return res.status(201).end();
+    }
 }
