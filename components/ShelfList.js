f0f4b0cfefdc6f9be7200c6cbc5928e2c33571b7
lissalad <elissalaymon@gmail.com>
2022-09-28 17:48:35 -0700

    styling profile page

--- components/ShelfList.js
+++ components/ShelfList.js
@@ -1,14 +1,37 @@
 import classNames from "classnames";
 import ShelfListItem from "./ShelfListItem";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function ShelfList(props) {
+  // const [tab, setTab] = useState("shelves");
+  const navTextStyle = "text-sm w-fit text-white rounded-t-sm py-2";
+  const tabStyle = "flex flex-col w-full border-t-2 divider hidden";
+
+  // function openTab(tabName) {
+  //   setTab()
+  // }
+
   return (
     <div className={classNames("flex flex-col items-start w-full rounded-sm","md:max-w-[500px]")}>
-      <h2 className={classNames("text-sm w-fit text-white bg-emerald-900 rounded-t-sm px-6 py-2 ","")}>Shelves</h2>
+
+      {/* NAV */}
+      <div className={classNames("flex flex-row space-x-10 px-4 w-full rounded-t-sm justify-center","md:justify-evenly")}>
+        <p className={navTextStyle}>Feed</p>
+        <p className={navTextStyle}>Shelves</p>
+        <p className={navTextStyle}>Reviews</p>
+        <p className={navTextStyle}>About</p>
+      </div>
+
+      {/* feed */}
+      <ul className={classNames("flex flex-col w-full border-t-2 divider hidden","")} id="feed">
+        <p>lissa read a book</p>
+        <p>lissa hated a book</p>
+        <p>lissa reviewed a book</p>
+      </ul>
 
       {/* shelf list */}
-      <ul className={classNames("flex flex-col w-full border-t-2 divider","")}>
+      <ul className={classNames("flex flex-col w-full border-t-2 divider","")} id="shelves">
 
         {/* example shelf */}
         {/* will need to be mapped, passed props and such */}
@@ -17,9 +40,9 @@ export default function ShelfList(props) {
         <ShelfListItem />
         <ShelfListItem />
         <ShelfListItem />
-
-
       </ul>
+
+
 
   </div>
   )
--- pages/my-profile.js
+++ pages/my-profile.js
@@ -15,45 +15,44 @@ function MyProfile(){
   const { user, isLoading } = useUser();
   const following = 43;
   const followers = 34;
+  console.log(user.picture);
 
 
   return (
     <main className={classNames("flex flex-col space-y-6","")}>
 
-      {/* basic profile info */}
-      <div className={classNames("flex flex-col items-start space-y-2 w-full","")}>
+      {/* profile row */}
+      <div className={classNames("flex flex-row items-end w-full space-x-3","md:space-x-6")}>
 
-        {/* first row */}
-        <div className={classNames("flex flex-row space-x-4 items-center"," md:w-full")}>
+        {/* profile picture */}
+        <img src={user.picture} className={classNames("rounded-sm w-16 shadow-2xl", "w-24")}></img>
 
-          {/* profile picture */}
-          <img src={user.picture} className={classNames("rounded-sm w-16 shadow-2xl", "w-24")}></img>
+
+        <div className={classNames("flex flex-col space-y-2","")}>
+
+          {/* name */}
+          <h1 className={classNames("text-2xl","")}>{user.name}</h1>
 
           {/* profile numbers */}
           <div className={classNames("flex flex-row space-x-4","")}>
 
             {/* follower count */}
-            <div className={classNames("flex flex-col items-center text-white/70","")}>
-              <h3 className={classNames("text-xs","")}>Followers</h3>
-              <p className={classNames("text-xs","")}>{followers}</p>
+            <div className={classNames("flex flex-row items-center text-white/70","")}>
+              <h3 className={classNames("text-xs","")}>{followers} Followers</h3>
             </div>
 
             {/* following count */}
             <div className={classNames("flex flex-col items-center text-white/70","")}>
-              <h3 className={classNames("text-xs","")}>Following</h3>
-              <p className={classNames("text-xs","")}>{following}</p>
+              <h3 className={classNames("text-xs","")}>{following} Following</h3>
             </div>
           </div>
 
         </div>
 
-        {/* second row*/}
-        <div className={classNames("flex flex-row","")}>
-          {/* name */}
-          <h1 className={classNames("text-2xl","")}>{user.name}</h1>
-        </div>
       </div>
 
+      {/* THESE TABS ARE IN PROGRESS */}
+      {/* ZERO LOGIC HERE, PLACEHOLDERS AS OF NOW */}
       {/* shelves */}
       <ShelfList />
 
