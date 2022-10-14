93932be551da0dafd8b055f693df5a1a1f8c50a0
root <root@DESKTOP-L53IOTU.localdomain>
2022-10-14 16:20:31 -0400

    Completed both follows and following route, returning the whole Follows table which will be parsed by the front-end

--- pages/api/users/[userId]/following.js
+++ pages/api/users/[userId]/following.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
     const all = prisma.Follows.findMany({
       where: {
-        followingId: userId,
+        followerId: userId,
       },
     });
 
--- pages/api/users/[userId]/follows.js
+++ pages/api/users/[userId]/follows.js
@@ -1,5 +1,18 @@
 import prisma from "../../../../lib/prisma";
 
 export default async function handler(req, res) {
-  //route to return all users who this user follows
+  //route to return who follows this user
+
+  //Check if user ID is provided
+  if (req.query.userId) {
+    var userId = parseInt(req.query.userId);
+
+    const all = prisma.Follows.findMany({
+      where: {
+        followingId: userId,
+      },
+    });
+
+    res.status(200).json(all);
+  }
 }
