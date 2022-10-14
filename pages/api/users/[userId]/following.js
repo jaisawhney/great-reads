db4fec3cdecddc5c2b4e76c5baf906e14d26d850
root <root@DESKTOP-L53IOTU.localdomain>
2022-10-14 16:19:03 -0400

    Completed following route

--- pages/api/users/[userId]/following.js
+++ pages/api/users/[userId]/following.js
@@ -2,4 +2,17 @@ import prisma from "../../../../lib/prisma";
 
 export default async function handler(req, res) {
   //route to return who is following this user
+
+  //Check if user ID is provided
+  if (req.query.userId) {
+    var userId = parseInt(req.query.userId);
+
+    const all = prisma.Follows.findMany({
+      where: {
+        followingId: userId,
+      },
+    });
+
+    res.status(200).json(all);
+  }
 }
