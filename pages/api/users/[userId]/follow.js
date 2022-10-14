f80939fe521d4735e6cac6c4e414e6d9237efdf0
root <root@DESKTOP-L53IOTU.localdomain>
2022-10-14 16:06:15 -0400

    Parsing user ID as int

--- pages/api/users/[userId]/follow.js
+++ pages/api/users/[userId]/follow.js
@@ -1,4 +1,4 @@
-import prisma from "prisma";
+import prisma from "../../../../lib/prisma";
 import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 
 export default withApiAuthRequired(FollowSomeone);
@@ -6,47 +6,35 @@ export default withApiAuthRequired(FollowSomeone);
 async function FollowSomeone(req, res) {
   const { user } = getSession(req, res);
   console.log(user);
-  if (req.method === "GET") {
-    //If get then just return ok
-    res.status(200).json({ ok: "opk" });
-  } else {
-    const userToFollow = req.query.userId;
 
-    //Check if user we wish to follow exists
+  const userToFollow = req.query.userId;
 
-    const existUser = await prisma.User.findFirst({
-      followingId: userToFollow,
-    });
-    //Return user not found
+  //Check if user we wish to follow exists
 
-    if (!existUser) return res.status(404).end();
+  const existUser = await prisma.User.findFirst({
+    where: {
+      followingId: parseInt(userToFollow),
+    },
+  });
+  //Return user not found
+  if (!existUser) return res.status(404).end();
 
-    // Check if this user is already following
-    /*const existingFolow = await prisma.follows.findFirst({
-          
-        });*/
-
-    // User exists (200)
-    if (existingFolow) return res.status(200).end();
-
-    //Add to the follows table
-
-    const newFollow = await prisma.Follows.create({
-      data: {
-        follower: {
-          connect: {
-            auth0Id: user.sub,
-          },
-        },
-        following: {
-          connect: {
-            id: userToFollow,
-          },
+  //Add to the follows table
+  const newFollow = await prisma.Follows.create({
+    data: {
+      follower: {
+        connect: {
+          auth0Id: user.sub,
         },
       },
-    });
+      following: {
+        connect: {
+          id: userToFollow,
+        },
+      },
+    },
+  });
 
-    // Follow added added (201)
-    return res.status(201).end();
-  }
+  // Follow added added (201)
+  return res.status(201).end();
 }
