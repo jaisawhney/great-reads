1dcd6aecb1ae7df905e8026878a6242b400f2d77
root <root@DESKTOP-L53IOTU.localdomain>
2022-10-14 16:03:14 -0400

    Working on follow api route, if target user does not exist return 404

--- pages/api/users/[userId]/follow.js
+++ pages/api/users/[userId]/follow.js
@@ -1,44 +1,52 @@
-import prisma from "../../../lib/prisma";
+import prisma from "prisma";
+import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 
-export default async function handler(req, res) {
-    if (req.method === "GET") {
-        //If get then just return ok
-        res.status(200)
-    } else {
-       
-        const userToFollow = req.query.userId
+export default withApiAuthRequired(FollowSomeone);
 
+async function FollowSomeone(req, res) {
+  const { user } = getSession(req, res);
+  console.log(user);
+  if (req.method === "GET") {
+    //If get then just return ok
+    res.status(200).json({ ok: "opk" });
+  } else {
+    const userToFollow = req.query.userId;
 
-      
+    //Check if user we wish to follow exists
 
-        // Check if this user is already following
-        const existingFolow = await prisma.follows.findFirst({
+    const existUser = await prisma.User.findFirst({
+      followingId: userToFollow,
+    });
+    //Return user not found
+
+    if (!existUser) return res.status(404).end();
+
+    // Check if this user is already following
+    /*const existingFolow = await prisma.follows.findFirst({
           
-        });
+        });*/
 
-        // User exists (200)
-        if (existingFolow) return res.status(200).end();
+    // User exists (200)
+    if (existingFolow) return res.status(200).end();
 
-        //Add to the follows table
-    
-        const newFollow = await prisma.follows.create({
-            data: {
-             
-              follower: {
-                connect: {
-                  id: sampleUserId
-                }
-              },
-              following: {
-                connect: {
-                  id: sampleBookId
-                }
-              }
-            },
-          });
-      
+    //Add to the follows table
 
-        // Follow added added (201)
-        return res.status(201).end();
-    }
+    const newFollow = await prisma.Follows.create({
+      data: {
+        follower: {
+          connect: {
+            auth0Id: user.sub,
+          },
+        },
+        following: {
+          connect: {
+            id: userToFollow,
+          },
+        },
+      },
+    });
+
+    // Follow added added (201)
+    return res.status(201).end();
+  }
 }
