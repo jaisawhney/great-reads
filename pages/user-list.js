74c6c82214a1336e0667af4b09ac5d15346a052a
root <root@DESKTOP-L53IOTU.localdomain>
2022-10-15 12:11:04 -0400

    Adding style to list and created follow button

--- pages/user-list.js
+++ pages/user-list.js
@@ -14,10 +14,17 @@ function userList(props) {
   });
   return (
     <>
-      This is the user list page
-      {users.map((user) => (
-        <>{user.email}</>
-      ))}
+      <div className="flex flex-col p-2 space-y-2">
+        <p className="text-3xl">All users</p>
+        {users.map((user) => (
+          <>
+            <div className="flex flex-row justify-between bg-slate-600 p-2 items-center rounded-md">
+              <p>{user.email}</p>
+              <button className="bg-sky-600 rounded-lg p-2">Follow</button>
+            </div>
+          </>
+        ))}
+      </div>
     </>
   );
 }
