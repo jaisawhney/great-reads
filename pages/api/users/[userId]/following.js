bad5ea7c11eec442e2c774055d4963766ca552f8
root <root@DESKTOP-L53IOTU.localdomain>
2022-10-15 12:20:49 -0400

    Updated following route to return all user deails, not just ID

--- pages/api/users/[userId]/following.js
+++ pages/api/users/[userId]/following.js
@@ -1,18 +1,32 @@
 import prisma from "../../../../lib/prisma";
 
 export default async function handler(req, res) {
-  //route to return who is following this user
+  //route to return who this user is following
 
   //Check if user ID is provided
   if (req.query.userId) {
     var userId = parseInt(req.query.userId);
 
-    const all = prisma.Follows.findMany({
+    const all = await prisma.Follows.findMany({
       where: {
         followerId: userId,
       },
     });
 
-    res.status(200).json(all);
+    var allUsers = [];
+
+    for (var i = 0; i < all.length; i++) {
+      allUsers.push(all[i].followingId);
+    }
+
+    const followingUsers = await prisma.user.findMany({
+      where: {
+        id: {
+          in: allUsers,
+        },
+      },
+    });
+
+    res.status(200).json(followingUsers);
   }
 }
