ffeb1e1ef51beb05a435d79a5d257270f0b0ff61
root <root@DESKTOP-L53IOTU.localdomain>
2022-10-16 09:21:01 -0400

    Created Followers, Following components for profile page

---
+++ components/Followers.js
@@ -0,0 +1,4 @@
+export default function Followers(props) {
+  //Just a page that list everyone we follow
+  return <>Follower page</>;
+}
---
+++ components/Following.js
@@ -0,0 +1,4 @@
+export default function Following(props) {
+  //Just a list of all users that this user is following
+  return <>Following page</>;
+}
