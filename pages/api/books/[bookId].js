20e705afcb04934d975db4a58fc0913ed5279d2d
Jai Sawhney <jai.sawhney1@gmail.com>
2022-10-17 11:17:53 -0700

    Create comment tree

--- pages/api/books/[bookId].js
+++ pages/api/books/[bookId].js
@@ -4,22 +4,35 @@ export default async function handler(req, res) {
   const bookId = parseInt(req.query.bookId);
 
   if (req.method === "GET") {
-    // Prisma doesn't support trees. The below will include up to two nested comments
     const book = await prisma.book.findUnique({
       where: { id: bookId },
       include: {
-        comments: {
-          where: {
-            parent: null,
-          },
-          include: {
-            parent: { include: { parent: true, replies: true } },
-            replies: { include: { parent: true, replies: true } },
-          },
-        },
+        comments: true,
       },
     });
 
+    // Create comment tree
+    const roots = [];
+    const commentMap = {};
+
+    for (let i = 0; i < book.comments.length; i += 1) {
+      const comment = book.comments[i];
+      comment.replies = [];
+      commentMap[comment.id] = i;
+    }
+
+    for (let i = 0; i < book.comments.length; i += 1) {
+      let comment = book.comments[i];
+      if (!comment.parentId) {
+        roots.push(comment);
+        continue;
+      }
+
+      const parent = book.comments[commentMap[comment.parentId]];
+      parent.replies = [...parent.replies, comment];
+    }
+
+    book.comments = roots;
     res.status(200).json(book);
   } else {
     const deletedBook = await prisma.book.delete({
