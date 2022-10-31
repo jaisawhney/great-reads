2a34f83948938895587dcf8d0effa5c70bb31d13
Jai Sawhney <jai.sawhney1@gmail.com>
2022-10-31 03:01:16 -0700

    Fix infinite loop

--- pages/user-list.js
+++ pages/user-list.js
@@ -1,5 +1,6 @@
 import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { useEffect, useState } from "react";
+
 export default withPageAuthRequired(userList);
 
 function userList(props) {
@@ -10,28 +11,28 @@ function userList(props) {
       const response = await fetch("/api/users").then((response) => response.json());
       setUsers(response);
     }
-    runGet();
-  });
+
+    runGet().catch(console.error);
+  }, []);
+
   return (
-    <>
+    <main>
       <div className="flex flex-col p-2 space-y-2">
         <p className="text-3xl">All users</p>
         {users.map((user) => (
-          <>
-            <div className="flex flex-row justify-between bg-slate-600 p-2 items-center rounded-md">
-              <p>{user.email}</p>
-              <button
-                className="bg-sky-600 hover:bg-sky-700 rounded-lg p-2"
-                onClick={() => {
-                  //Follow this user
-                  fetch("/api/users/" + user.id + "/follow").then((response) => response.json());
-                }}>
-                Follow
-              </button>
-            </div>
-          </>
+          <div className="flex flex-row justify-between bg-slate-600 p-2 items-center rounded-md">
+            <p>{user.email}</p>
+            <button
+              className="bg-sky-600 hover:bg-sky-700 rounded-lg p-2"
+              onClick={() => {
+                //Follow this user
+                fetch("/api/users/" + user.id + "/follow").then((response) => response.json());
+              }}>
+              Follow
+            </button>
+          </div>
         ))}
       </div>
-    </>
+    </main>
   );
 }
