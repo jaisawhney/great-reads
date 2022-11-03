1b2f289d403847f712ce0daba610c52c578583fc
Lartg <roc.gerrety@gmail.com>
2022-11-03 08:26:15 -0800

    protected api routes

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,16 +1,18 @@
-export default async function handler(req, res) {
-    try {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
+import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 
-        const result = await fetch(
-            `http://openlibrary.org/search.json?q=${req.query.q}&fields=key,title,author_name,cover_i,number_of_pages_median,first_publish_year&page=1`
-        );
-        const unparsedJson = await result.text();
-        res.end(unparsedJson);
-    } catch (err) {
-        res.json(err);
-        res.status(500);
-        res.end();
-    }
-}
+export default withApiAuthRequired(async (req, res) => {
+  try {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+
+    const result = await fetch(
+      `http://openlibrary.org/search.json?q=${req.query.q}&fields=key,title,author_name,cover_i,number_of_pages_median,first_publish_year&page=1`
+    );
+    const unparsedJson = await result.text();
+    res.end(unparsedJson);
+  } catch (err) {
+    res.json(err);
+    res.status(500);
+    res.end();
+  }
+});
