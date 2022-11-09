eff97072a968e4c6504044fabd3b0f23bdb445f0
Jai Sawhney <jai.sawhney1@gmail.com>
2022-11-09 04:11:21 -0800

    Add component

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,18 +1,34 @@
 import classNames from "classnames";
+import SearchIcon from "../public/icons/SearchIcon";
+import { useRouter } from "next/router";
 
 export default function SearchBar() {
+  const router = useRouter();
+
+  // Redirect to the search page on submit
+  function searchSubmit(e) {
+    e.preventDefault();
+    const bookName = e.target.elements.bookName.value;
+    router.push(`/add-book?title=${bookName}`);
+  }
+
   return (
     <form
       onSubmit={searchSubmit}
       className={classNames(
-        "border rounded shadow-sm bg-white text-black flex w-full flex-row justify-between px-2 py-1",
-        "md:mb-6"
+        "border rounded shadow-sm bg-white text-black flex w-full flex-row justify-between",
+        "md:mb-6 md:w-[300px]"
       )}>
-      <input className="" name="bookName" type="text" placeholder="Book Title" required />
-      <button className={classNames("")} type="submit">
-        <Search />
+      <input
+        className={classNames("w-full px-2 py-1")}
+        name="bookName"
+        type="text"
+        placeholder="Book Title"
+        required
+      />
+      <button className={classNames("mx-1")} type="submit">
+        <SearchIcon />
       </button>
-      {/* <input className="px-2 w-fit" type="submit" /> */}
     </form>
   );
 }
