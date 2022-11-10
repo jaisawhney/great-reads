f1cc1115058eae94caa120e5c0fa9a15c3d93262
Lartg <roc.gerrety@gmail.com>
2022-11-10 00:59:48 -0900

    styling done

---
+++ components/BackButton.js
@@ -0,0 +1,8 @@
+import classNames from "classnames";
+import BackArrow from "../public/icons/BackArrow";
+
+<Link href="/path/to/place" className={classNames("")}>
+  <div>
+    <BackArrow />
+  </div>
+</Link>;
