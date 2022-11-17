9ea9b2df1b6dbcdae7524bd878ecc4718c10bbd5
Jai Sawhney <jai.sawhney1@gmail.com>
2022-11-17 10:30:17 -0800

    Style

--- components/NotificationBar.js
+++ components/NotificationBar.js
@@ -1,9 +1,19 @@
 import classNames from "classnames";
+import BannerCloseIcon from "./icons/BannerCloseIcon";
 
 export default function NotificationBar(props) {
   return (
-    <div className={classNames(props.isVisible ? "" : "hidden")}>
-      <p>{props.message}</p>
+    <div className={classNames("bg-teal-800 w-100 mx-auto p-2", "")}>
+      <div className={classNames("flex items-center justify-between")}>
+        <div className={classNames("flex flex-shrink-0")}>
+          <p>{props.message}</p>
+        </div>
+        <div className={classNames("flex-shrink-0")}>
+          <button type="button" className={classNames("flex rounded p-0.5 hover:bg-teal-700")}>
+            <BannerCloseIcon />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
