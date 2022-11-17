83f7e7d7e32165f323c9a8ba4a64ad0d8f622d49
Jai Sawhney <jai.sawhney1@gmail.com>
2022-11-17 11:16:44 -0800

    Working close icon

--- components/NotificationBar.js
+++ components/NotificationBar.js
@@ -1,15 +1,25 @@
 import classNames from "classnames";
 import BannerCloseIcon from "./icons/BannerCloseIcon";
+import { useState } from "react";
 
 export default function NotificationBar(props) {
+  const [isVisible, setVisibility] = useState(true);
+
+  function hideBanner() {
+    setVisibility(false);
+  }
+
   return (
-    <div className={classNames("bg-teal-800 w-100 mx-auto p-2", "")}>
+    <div className={classNames(isVisible ? "" : "hidden", "bg-teal-800 w-100 mx-auto p-2", "")}>
       <div className={classNames("flex items-center justify-between")}>
         <div className={classNames("flex flex-shrink-0")}>
           <p>{props.message}</p>
         </div>
         <div className={classNames("flex-shrink-0")}>
-          <button type="button" className={classNames("flex rounded p-0.5 hover:bg-teal-700")}>
+          <button
+            type="button"
+            className={classNames("flex rounded p-0.5 hover:bg-teal-700")}
+            onClick={hideBanner}>
             <BannerCloseIcon />
           </button>
         </div>
