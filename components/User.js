fcbd83a2468398ac45cafd4ebabfc8050a01a9b4
Jai Sawhney <jai.sawhney1@gmail.com>
2022-11-29 16:01:07 -0800

    Update user list components

--- components/User.js
+++ components/User.js
@@ -1,13 +1,17 @@
 export default function User(props) {
+  const isFollowing = props.usersFollowing.find((user) => user.id === props.user.id);
   return (
     <div className="flex flex-row justify-between bg-slate-600 p-2 items-center rounded-md">
       <p>{props.user.username}</p>
+
       <button
         className="bg-sky-600 hover:bg-sky-700 rounded-lg p-2"
         onClick={() => {
-          fetch("/api/users/" + props.user.id + "/follow").then((response) => response.json());
+          fetch("/api/users/" + props.user.id + `/${isFollowing ? "unfollow" : "follow"}`).then(
+            () => props.refreshFollowing()
+          );
         }}>
-        Follow
+        {isFollowing ? "Unfollow" : "Follow"}
       </button>
     </div>
   );
--- components/UserList.js
+++ components/UserList.js
@@ -4,6 +4,13 @@ export default function UserList(props) {
   return props.users
     .filter((user) => user.id !== props.user.internalId)
     .map((user) => {
-      return <User user={user} key={user.id} />;
+      return (
+        <User
+          user={user}
+          usersFollowing={props.usersFollowing}
+          refreshFollowing={props.refreshFollowing}
+          key={user.id}
+        />
+      );
     });
 }
